6976558ed15597c4e59cd443180186c473bef342
Konstantin Suslin <ksuslin@gmail.com>
2025-05-21 02:13:25 +0300

    ALmost done

--- Assets/_HW1/Scripts/Bomber.cs
+++ Assets/_HW1/Scripts/Bomber.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Bomber : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float _radius;
+    [SerializeField] private float _explosionForce;
+    [SerializeField] private LayerMask _layerMask;
+    
+    private void Update()
     {
-        
+        if (Input.GetMouseButtonDown(1))
+            Bomb();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Bomb()
     {
-        
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z));
+
+        Vector3 explosionPoint = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z));
+
+        if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, _layerMask))
+        {
+            Collider[] targets = Physics.OverlapSphere(hit.point, _radius);
+
+            foreach (Collider target in targets)
+            {
+                Rigidbody rigidbody = target.GetComponent<Rigidbody>();
+
+                if (rigidbody != null)
+                {
+                    // Применяем силу взрыва
+                    rigidbody.AddExplosionForce(_explosionForce, explosionPoint, _radius, 1f, ForceMode.Impulse);
+                }
+            }
+        }
     }
 }
--- Assets/_HW1/Scripts/DragNDropper.cs
+++ Assets/_HW1/Scripts/DragNDropper.cs
@@ -1,3 +1,4 @@
+using UnityEditor.PackageManager;
 using UnityEngine;
 
 public class DragNDropper : MonoBehaviour
@@ -8,6 +9,7 @@ public class DragNDropper : MonoBehaviour
 
     private float _coefficiency;
     private Transform _objectTransform;
+    private Vector3 _objectScale;
 
     private void Update()
     {
@@ -16,7 +18,12 @@ public class DragNDropper : MonoBehaviour
             Ray cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
 
             if (Physics.Raycast(cameraRay, out RaycastHit hitInfo, Mathf.Infinity, _layerMask))
+            {
                 _objectTransform = hitInfo.transform;
+                _objectScale = hitInfo.transform.localScale;
+                hitInfo.transform.localScale = _objectScale * 1.2f;
+            }
+                
         }
 
         if (Input.GetMouseButton(LeftMouseButton))
@@ -28,14 +35,26 @@ public class DragNDropper : MonoBehaviour
                 if (_coefficiency == 0)
                     _coefficiency = point.y - _objectTransform.position.y;
 
-                _objectTransform.position = new Vector3(point.x, point.y - _coefficiency, _objectTransform.position.z);                
+                _objectTransform.position = new Vector3(point.x, point.y - _coefficiency, _objectTransform.position.z);
+
+                if (_objectTransform.position.y <= 0.5f)
+                {
+                    _objectTransform.position = new Vector3(_objectTransform.position.x, 0.5f, _objectTransform.position.z); 
+                }
+
+                    
             }            
         }
 
         if (Input.GetMouseButtonUp(LeftMouseButton))
         {
             _coefficiency = 0;
-            _objectTransform = null;
+            
+            if(_objectTransform != null)
+            {
+                _objectTransform.localScale = _objectScale;
+                _objectTransform = null;
+            }            
         }
     }
 }
